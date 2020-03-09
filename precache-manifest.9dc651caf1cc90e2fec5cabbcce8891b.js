d413caeb3032918a16a4f5de2477dc78087f8f71
jackklop <jackklop123@gmail.com>
2020-03-08 23:21:37 -0400

    Updates

---
+++ precache-manifest.9dc651caf1cc90e2fec5cabbcce8891b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd7fd5789c80ad5b76ef61be2b860418",
+    "url": "/Restaurant-Roulette/index.html"
+  },
+  {
+    "revision": "4464b5ed810a416446e1",
+    "url": "/Restaurant-Roulette/static/css/main.1d373d7f.chunk.css"
+  },
+  {
+    "revision": "81a3190b4ef6413fcebe",
+    "url": "/Restaurant-Roulette/static/js/2.3f75b8ee.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Restaurant-Roulette/static/js/2.3f75b8ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4464b5ed810a416446e1",
+    "url": "/Restaurant-Roulette/static/js/main.b1775ce7.chunk.js"
+  },
+  {
+    "revision": "30858712bf9ab1fe997e",
+    "url": "/Restaurant-Roulette/static/js/runtime-main.b8c7a951.js"
+  },
+  {
+    "revision": "b8943aa4c603dff29ae13aebbb7bee22",
+    "url": "/Restaurant-Roulette/static/media/hungry-cat.b8943aa4.jpg"
+  }
+]);
